ad8e5d5374bb911d3715c3e3c4d8bee398f764d8
namseungsik <tmd7031@naver.com>
2022-02-04 16:23:33 +0900

    this, target

---
+++ src/test/java/hello/aop/pointcut/ParameterTest.java
@@ -0,0 +1,84 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.annotation.ClassAop;
+import hello.aop.member.annotation.MethodAop;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(ParameterTest.ParameterAspect.class)
+@SpringBootTest
+public class ParameterTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memverService Proxy = {}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class ParameterAspect {
+
+        @Pointcut("execution(* hello.aop.member..*.*(..))")
+        private void allMember() {
+        }
+
+        @Around("allMember()")
+        public Object logArg1(ProceedingJoinPoint joinPoint) throws Throwable {
+            Object arg1 = joinPoint.getArgs()[0];
+            log.info("[logArg1]{}, arg={}", joinPoint.getSignature(), arg1);
+            return joinPoint.proceed();
+        }
+
+        @Around("allMember() && args(arg, ..)")
+        public Object logArg2(ProceedingJoinPoint joinPoint, Object arg) throws Throwable {
+            Object arg2 = joinPoint.getArgs()[0];
+            log.info("[logArg2]{}, arg={}", joinPoint.getSignature(), arg2);
+            return joinPoint.proceed();
+        }
+
+        @Before("allMember() && args(arg, ..)")
+        public void logArg3(String arg) { // 타입 매칭이 안되면 실행 안됨.
+            log.info("[logArg3] arg={}", arg);
+        }
+
+        @Before("allMember() && this(obj)")
+        public void thisArgs(JoinPoint joinPoint, MemberService obj) {
+            log.info("[this]{}, obj = {}", joinPoint.getSignature(), obj.getClass()); // obj는 프록시 객체
+        }
+
+        @Before("allMember() && target(obj)")
+        public void targetArgs(JoinPoint joinPoint, MemberService obj) {
+            log.info("[target]{}, obj = {}", joinPoint.getSignature(), obj.getClass()); // obj는 실제 객체 인스턴스
+        }
+
+        @Before("allMember() && @target(annotation)")
+        public void atTargetArgs(JoinPoint joinPoint, ClassAop annotation) {
+            log.info("[@target]{}, obj = {}", joinPoint.getSignature(), annotation);
+        }
+
+        @Before("allMember() && @within(annotation)")
+        public void atWithinArgs(JoinPoint joinPoint, ClassAop annotation) {
+            log.info("[@within]{}, obj = {}", joinPoint.getSignature(), annotation);
+        }
+
+        @Before("allMember() && @annotation(annotation)")
+        public void atAnnotationArgs(JoinPoint joinPoint, MethodAop annotation) {
+            log.info("[@annotation]{}, annotationValue = {}", joinPoint.getSignature(), annotation.value());
+        }
+    }
+}
