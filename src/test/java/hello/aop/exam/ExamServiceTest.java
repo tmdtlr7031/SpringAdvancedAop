11b9ca9eabc10b95e8371f8af846e0ad7628a706
namseungsik <tmd7031@naver.com>
2022-02-07 20:53:45 +0900

    스프링 AOP 실전 예제 - 로그출력 AOP

--- src/test/java/hello/aop/exam/ExamServiceTest.java
+++ src/test/java/hello/aop/exam/ExamServiceTest.java
@@ -1,13 +1,16 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
+@Import(TraceAspect.class)
 @SpringBootTest
 public class ExamServiceTest {
 
